e63e463a036c534137ec04965ff225a4c42e886a
Himanshu <shimanshu45@gmail.com>
2020-01-23 22:42:59 +0530

    ++ created new layout file add_new_plant

--- app/src/main/java/com/example/easygardening/MainActivity.java
+++ app/src/main/java/com/example/easygardening/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.easygardening;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] Array = {"Fruit", "Vegetable", "Flower"};
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_add_new_plant);
+
+        spinner = findViewById(R.id.sp_typeOfPlant);
+
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, Array));
     }
 }
